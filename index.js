8b1db62acf63e80ea69c74e5cc13729d470dd600
Jeremy Jones <jeremy.jones@getbuilt.com>
2015-10-06 18:39:41 -0500

    add success callback param to weather module. move weather call to route handler

--- index.js
+++ index.js
@@ -10,16 +10,6 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-errorHandler = function (){
-	console.log('got some error')
-}
-
-//current weather takes pin code or location as first parameter, error handler callback as second
-//YO JEREMY -
-//-- THIS IS WHAT GETS BACK AN ARRAY OF OBJECTS THAT I WANT TO ACCESS
-weather.currentWeather('Nashville', errorHandler);
-
-
 //forecast weather takes pin code or location as first parameter, number of days as second, error handler callback as second
 //weather.forecastWeather(20500, 2, errorHandler);
 
@@ -30,7 +20,11 @@ app.get('/', function(request, response) {
 
 //Weather page
 app.get('/weather', function(request, response) {
-  response.render('pages/weather', {weather: weather});
+	weather.currentWeather('Nashville', function(weather) {
+		response.render('pages/weather', {weather: weather});
+	}, function(err) {
+		res.status(500).json(err);
+	});
 });
 
 app.listen(app.get('port'), function() {
