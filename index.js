30b83cd49e31900c3147f0741b0a4b444d3ba07f
Jordan McArdle <Jordan.McArdle@2-com.net>
2015-10-06 17:27:25 -0500

    Review jeremy help me :P

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var weather = require('./lib/weatherlib.js');
 var express = require('express');
 var app = express();
 
@@ -9,12 +10,27 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+errorHandler = function (){
+	console.log('got some error')
+}
+
+//current weather takes pin code or location as first parameter, error handler callback as second
+//YO JEREMY -
+//-- THIS IS WHAT GETS BACK AN ARRAY OF OBJECTS THAT I WANT TO ACCESS
+weather.currentWeather('Nashville', errorHandler);
+
+
+//forecast weather takes pin code or location as first parameter, number of days as second, error handler callback as second
+//weather.forecastWeather(20500, 2, errorHandler);
+
+//Home page
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+//Weather page
 app.get('/weather', function(request, response) {
-  response.render('pages/weather');
+  response.render('pages/weather', {weather: weather});
 });
 
 app.listen(app.get('port'), function() {
